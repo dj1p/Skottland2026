4d78c61106beaafa2e592151618293dc133f8ba6
Dj1p <46656445+dj1p@users.noreply.github.com>
2025-11-28 11:56:12 +0700

    Update TripPage.tsx

--- src/pages/TripPage.tsx
+++ src/pages/TripPage.tsx
@@ -552,7 +552,7 @@ export default function TripPage({ trip }: TripPageProps) {
                   <div className="flex items-center justify-between">
                     <div>
                       <p className="text-stone-100 font-medium">Totalt estimat</p>
-                      <p className="text-stone-500 text-sm">Golf: £340 + Hus: 5 213 NOK + Mat: ca 500 NOK (Frokost&Middag)</p>
+                      <p className="text-stone-500 text-sm">Golf: £340 + Hus: 5 213 NOK + Mat: ca 500 NOK (Frokost&Middag)<br>vs 2024: Ca 12.000 p,p</p>
                     </div>
                     <div className="text-emerald-400 font-bold text-xl">~10 500 NOK</div>
                   </div>
